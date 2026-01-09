ded33840cea2bd2e4e4695ac1a71a185bc48b414
Blagoja <blagojaan@yahoo.com>
2026-01-09 19:24:11 +0100

    Produkt in Warenkorb erhöhen, reduzieren und Löschen

---
+++ src/test/java/de/ziyadem/step_definitions/MengeInwarenkorbErhöhen_Step_Defs.java
@@ -0,0 +1,59 @@
+package de.ziyadem.step_definitions;
+
+import de.ziyadem.pages.*;
+import de.ziyadem.utils.BrowserUtils;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class MengeInwarenkorbErhöhen_Step_Defs {
+
+    LoginPage loginPage = new LoginPage();
+
+    DashboardPage dashboardPage = new DashboardPage();
+
+    OlivensortenPage olivensortenPage = new OlivensortenPage();
+
+    ProdactPage prodactPage = new ProdactPage();
+
+    WarenkorbPage warenkorbPage = new WarenkorbPage();
+
+
+    @When("hoveringr over auf Delikatessen und click on the Olivensorten")
+    public void hoveringr_over_auf_delikatessen_und_click_on_the_olivensorten() {
+        BrowserUtils.hover(dashboardPage.delicatessenLink);
+        dashboardPage.olivensorten.click();
+
+    }
+    @When("click on the fourth Prodact")
+    public void click_on_the_fourth_prodact() {
+        BrowserUtils.clickWithJS(olivensortenPage.cocktailOlivenLink);
+
+    }
+    @When("uaer clicks on the In Warenkorb Button")
+    public void uaer_clicks_on_the_in_warenkorb_button() {
+        prodactPage.inDenWarenkorbButton.click();
+
+    }
+    @When("user clicks on the Warenkorb Icon")
+    public void user_clicks_on_the_warenkorb_icon() {
+        prodactPage.warenkorbIcon.click();
+    }
+    @When("user increases the quantity on two")
+    public void user_increases_the_quantity_to() {
+        warenkorbPage.increaseQuantityButton.click();
+    }
+    @When("user clicks on the Aktualisieren Button")
+    public void user_clicks_on_the_aktualisieren_button() {
+        warenkorbPage.aktualisierenButton.click();
+    }
+    @Then("Verify that the product quantity is increased in the shopping cart.")
+    public void verify_that_the_product_quantity_is_increased_in_the_shopping_cart() {
+        String expectedQuantity = "2";
+        String actualQuantity = warenkorbPage.cocktailOlivenWarenkorb.getText();
+        System.out.println("actualQuantity = " + actualQuantity);
+        BrowserUtils.sleep(2);
+        warenkorbPage.removeItemButton.click();
+        BrowserUtils.sleep(2);
+    }
+
+}
